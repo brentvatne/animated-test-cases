12e24fa43bd2e1f203edaa983471f1d75348c03a
Brent Vatne <brentvatne@gmail.com>
2015-12-28 15:49:08 -0800

    Simple example and comments to index

--- index.android.js
+++ index.android.js
@@ -2,4 +2,8 @@
 
 import { AppRegistry } from 'react-native';
 
-AppRegistry.registerComponent('SimpleReddit', () => require('./App/Examples/NavigationJankyExample'));
+AppRegistry.registerComponent('SimpleReddit', () => require('./App/Examples/SuperSimpleExample'));
+
+// comment the SuperSimpleExample above and uncomment the NavigationJankyExample below
+// to test it out with Animated powered Navigator
+// AppRegistry.registerComponent('SimpleReddit', () => require('./App/Examples/NavigationJankyExample'));
